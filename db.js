b7dc097433823fe006ac0529fb2a64f807af3046
Manasa-008 <manasapenikalapati@gmail.com>
2026-08-18 13:07:07 +0000

    Fix Cognodb environment variables

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const neo4j = require("neo4j-driver");
 require("dotenv").config();
 
 const driver = neo4j.driver(
-    process.env.COGNO_URI,
+    process.env.COGNODB_URI,
     neo4j.auth.basic(
-        process.env.COGNO_USER,
-        process.env.COGNO_PASSWORD
+        process.env.COGNODB_USERNAME,
+        process.env.COGNODB_PASSWORD
     )
 );
 
